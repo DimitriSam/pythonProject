d6777bafe82117d2fde6febbf6b7f586afadb934
Dimitris Samouil <dimitrissamouil@Dimitriss-MacBook-Pro-2.local>
2023-01-25 15:06:59 +0200

    Fix spelling

--- dbt/models/test_script.sql
+++ dbt/models/test_script.sql
@@ -1,2 +1,2 @@
-select cus.name, cus.last_name, date_created , amoun from customers cus
+select cus.name, cus.last_name, date_created , amount from customers cus
 inner Join transaction tr ON tr.id = cus.customer_id
