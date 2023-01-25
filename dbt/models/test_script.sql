d5c34c53941e3e04131b40d938b32a60e7b61fbc
Dimitris Samouil <dimitrissamouil@Dimitriss-MacBook-Pro-2.local>
2023-01-25 15:25:44 +0200

    Fix bash command

--- dbt/models/test_script.sql
+++ dbt/models/test_script.sql
@@ -1,2 +1,3 @@
-select cus.name, cus.last_name, date_created , amount from customers cus
+select cus.name, cus.last_name,
+date_created , amount from customers cus
 inner Join transaction tr ON tr.id = cus.customer_id
