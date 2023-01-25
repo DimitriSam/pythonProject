1d9dc28912b3682eb382e9c371753caa743b16c5
Dimitris Samouil <dimitrissamouil@Dimitriss-MacBook-Pro-2.local>
2023-01-25 17:08:19 +0200

    Fix SQL format

--- dbt/models/test_script.sql
+++ dbt/models/test_script.sql
@@ -1,2 +1,7 @@
-select cus.name, cus.last_name,date_created ,  amount from customers cus
-inner Join transaction tr ON tr.id = cus.customer_id
+select
+    cus.name,
+    cus.last_name,
+    cus.date_created,
+    cus.amount
+from customers as cus
+inner join transaction as tr on tr.id = cus.customer_id
