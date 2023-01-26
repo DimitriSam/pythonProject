34cb53ace0d97f35edc963f9109111edccf8924b
Dimitris Samouil <dimitrissamouil@Dimitriss-MBP-2.localdomain>
2023-01-26 11:31:18 +0200

    Fix SQL format

--- dbt/models/test_script.sql
+++ dbt/models/test_script.sql
@@ -5,4 +5,4 @@ SELECT
     , cus.amount
     , tr.transaction_date
 FROM customers AS cus
-INNER JOIN transaction AS tr ON tr.id = cus.customer_id INNER JOIN transaction AS tr ON tr.id = cus.customer_id INNER JOIN transaction AS tr ON tr.id = cus.customer_id
+INNER JOIN transaction AS tr ON tr.id = cus.customer_id INNER join transaction AS tr ON tr.id = cus.customer_id INNER JOIN transaction AS tr ON tr.id = cus.customer_id
