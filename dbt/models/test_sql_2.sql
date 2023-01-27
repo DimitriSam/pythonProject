a045302925445602d1e2767fd6bbb14e257ede4c
Dimitris Samouil <dimitrissamouil@Dimitriss-MacBook-Pro-2.local>
2023-01-27 11:57:58 +0200

    test

--- dbt/models/test_sql_2.sql
+++ dbt/models/test_sql_2.sql
@@ -1,3 +1,4 @@
 SELECT cus.name
 FROM customers cus
 INNER JOIN employees em ON em.id = cus.id
+--I want to check again that nothing is running before the PR;
--- sql_file.sql
+++ sql_file.sql
@@ -1,1 +1,2 @@
 select something from random_table;
+--I want to check again that nothing is running before the PR;
