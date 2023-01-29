2e412fe3f115eb98997902264c52a23c3b5330bf
DimitriSam <44145365+DimitriSam@users.noreply.github.com>
2023-01-29 18:16:36 +0200

    Test pull request (#8)
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test 29JAN
    
    * test 29JAN
    
    * Change and PR
    
    * Change and PR
    
    * Change and PR
    
    ---------
    
    Co-authored-by: Dimitris Samouil <dimitrissamouil@Dimitriss-MBP-2.localdomain>
    Co-authored-by: Dimitris Samouil <dimitrissamouil@Dimitriss-MacBook-Pro-2.local>

--- dbt/models/test_script.sql
+++ dbt/models/test_script.sql
@@ -7,3 +7,4 @@ SELECT
 FROM customers AS cus
 INNER JOIN transactions AS tr ON cus.customer_id = tr.id
 WHERE (cus.date_created < NOW() - 1 AND tr.amount > 10 AND tr.amount IS NOT NULL);
+--put some text here that denotes the change before PR;
