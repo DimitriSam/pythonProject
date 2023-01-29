625b8106e1285bfbeafea0b852038d86b6984667
Dimitris Samouil <dimitrissamouil@Dimitriss-MacBook-Pro-2.local>
2023-01-29 20:09:45 +0200

    Fix violations

--- dbt/models/test_script.sql
+++ dbt/models/test_script.sql
@@ -6,4 +6,4 @@ SELECT
     , tr.transaction_date
 FROM customers AS cus
 INNER JOIN transactions AS tr ON cus.customer_id = tr.id
-WHERE (cus.date_created < NOW() - 1 AND tr.amount > 10 AND tr.amount IS NOT NULL)
+WHERE (cus.date_created < NOW() - 1 AND tr.amount > 10 AND tr.amount IS NOT NULL);
